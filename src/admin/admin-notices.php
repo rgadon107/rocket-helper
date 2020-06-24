917bcf56137010dd377aa096cfbb7057e2034814
Robert Gadon <rgadon107@gmail.com>
2020-06-23 23:23:30 -0500

    Add 'admin-notices.php' file. Unregister first admin notice.

---
+++ src/admin/admin-notices.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  Handle admin notices displays in the WP Rocket plugin
+ *
+ * @package    spiralWebDb\rocketHelper\Admin
+ * @since      1.0.0
+ * @author     Robert A. Gadon
+ * @link       http://spiralwebdb.com
+ * @license    GNU General Public License 2.0+
+ */
+
+namespace spiralWebDb\rocketHelper\Admin;
+
+use WP_Rocket\Engine\Cache\AdvancedCache;
+
+add_action( 'in_admin_header', __NAMESPACE__ . '\unregister_htaccess_permissions_notice' );
+/**
+ * Unregister the htaccess permissions admin notice in the WP Rocket plugin.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function unregister_htaccess_permissions_notice() {
+	remove_action( 'admin_notices', 'rocket_warning_htaccess_permissions' );
+}
+
